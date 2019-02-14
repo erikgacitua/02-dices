d4f6b560c513fc395d26b62eb9d8e100058acd4f
Erik Felipe Gacitua Arenas <exegaci@MacBook-Pro-de-Erik-2.local>
2019-02-14 10:09:21 -0300

    Initial Commit

---
+++ 02-dices/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  02-dices
+//
+//  Created by Erik Felipe Gacitua Arenas on 2/14/19.
+//  Copyright © 2019 Erik Felipe Gacitua Arenas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
